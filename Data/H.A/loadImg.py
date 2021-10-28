6d65b0e988e32a5292d87ef98879906d5ff9ea60
AnhNH <toshirou.ha@gmail.com>
2021-10-28 14:25:25 +0700

    232 img week2

--- Data/H.A/loadImg.py
+++ Data/H.A/loadImg.py
@@ -3,7 +3,7 @@ lines = file.readlines()
 imgName = []
 for indx,line in enumerate(lines) :
     if (indx%5 == 0) :
-        img = line[:line.rfind("#0")]
+        img = line[:line.rfind("| 0|")]
         imgName.append(img)
 print(imgName)
 print(len(imgName))
@@ -14,6 +14,7 @@ sourcePath = 'D:/FPTedu/AI/flickr30k_images'
 import os
 import shutil
 import time
+from numpy import random
 
 
 
@@ -21,7 +22,17 @@ files = os.listdir(sourcePath)
 for f in files :
     for img in imgName :
         if (f.startswith(img)) and (img != '') : 
-            
-            shutil.move(sourcePath+"/"+img,destinationPath+"/"+img)
+            ms = str(random.randint(1000,99999999))
+            strings = time.strftime("%Y%m%d%m")[2:]
+            newImgName = strings + ms[:4] + '_89.jpg'
+            shutil.move(sourcePath+"/"+img,destinationPath+"/"+newImgName)
+            file = open("Data/H.A/processToken.txt",'r')
+            line2 = file.readlines()
+            for indx,line in enumerate(line2) :
+                if(line.__contains__(img)):
+                    line2[indx] = newImgName + line
+                    out = open("Data/H.A/processToken.txt",'w')
+                    out.writelines(line2)
+                    out.close()
             
 
